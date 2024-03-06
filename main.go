bb84a64e12f9b2e0d3cf34c75226da3acc8c1f57
xnacly <47723417+xNaCly@users.noreply.github.com>
2024-03-06 08:17:29 +0100

    main: cleaned logging up

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 	"time"
 )
 
-const TIMEOUT = time.Millisecond * 50
-
 var FILE_CACHE = map[string]time.Time{}
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, nil)))
 	command := flag.String("c", "", "Command to execute if a change to the watched files was detected")
 	files := flag.String("f", "", "File extensions to watch for seperated by ;, leave empty for all")
+	timeout := flag.Duration("d", time.Millisecond*100, "Timeout for checking all files for changes")
 	flag.Parse()
 	if *command == "" {
-		slog.Error("Invalid command", "err", "Missing command, please specifiy")
+		slog.Error("Command Missing")
 		os.Exit(1)
 	}
 	extensions := make([]string, 0, 2)
@@ -36,7 +36,7 @@ func main() {
 		defer wg.Done()
 		for {
 			file := <-hasChange
-			slog.Info("Detected change, triggering command", "filename", file, "command", *command)
+			slog.Info("Change detected, triggering command", "file", file)
 			err := execute(*command)
 			if err != nil {
 				slog.Error("Failed to execute command", "err", err)
@@ -46,17 +46,23 @@ func main() {
 
 	wg.Add(1)
 	go func() {
+		c := 0
 		defer wg.Done()
 		for {
 			for _, file := range walkDir(".", extensions) {
 				if f, ok := FILE_CACHE[file.Path]; !ok {
 					FILE_CACHE[file.Path] = file.LastMod
+					c++
 				} else if file.LastMod.After(f) {
 					hasChange <- file.Path
 					FILE_CACHE[file.Path] = file.LastMod
 				}
 			}
-			time.Sleep(TIMEOUT)
+			if c > 0 {
+				slog.Info("Watching for changes", "files", c, "command", *command, "timeout", *timeout)
+				c = 0
+			}
+			time.Sleep(*timeout)
 		}
 	}()
 
